42263f98ea97f17b80c018dfe726dfc575bcfb87
Ted <oancea.tudor@icloud.com>
2023-12-03 19:48:10 +0800

    part 2 of day 2

--- src/day02.zig
+++ src/day02.zig
@@ -23,7 +23,8 @@ const maxRed = 12;
 const maxGreen = 13;
 const maxBlue = 14;
 
-fn check(gameRounds: String) !bool {
+fn check(gameRounds: String) ![3]u8 {
+    var result = [3]u8{ 0, 0, 0 };
     // split the game into rounds by the semicolon
     var rounds = splitSca(u8, gameRounds, ';');
     // for each round, split into the colors by the comma
@@ -35,29 +36,45 @@ fn check(gameRounds: String) !bool {
             const sep = lastIndexOf(u8, color, ' ') orelse unreachable;
             const num = try parseInt(u8, color[1..sep], 10);
             switch (color[sep + 1]) {
-                'r' => if (num > maxRed) return false,
-                'g' => if (num > maxGreen) return false,
-                'b' => if (num > maxBlue) return false,
+                'r' => {
+                    if (num > result[0]) {
+                        result[0] = num;
+                    }
+                },
+                'g' => {
+                    if (num > result[1]) {
+                        result[1] = num;
+                    }
+                },
+                'b' => {
+                    if (num > result[2]) {
+                        result[2] = num;
+                    }
+                },
                 else => unreachable,
             }
         }
     }
-    return true;
+    return result;
 }
 
 pub fn main() !void {
     var games = splitSca(u8, data, '\n');
     var validGames: u32 = 0;
+    var powerSum: u32 = 0;
     while (games.next()) |game| {
         const colonIndex = indexOf(u8, game, ':') orelse unreachable;
         const gameRounds = game[(colonIndex + 1)..];
-        if (try check(gameRounds)) {
+        const res = try check(gameRounds);
+        powerSum += @as(u32, res[0]) * @as(u32, res[1]) * @as(u32, res[2]);
+        if (res[0] <= maxRed and res[1] <= maxGreen and res[2] <= maxBlue) {
             // cast the slice game[5..colonIndex] to a String
             const gameIndex = try parseInt(u8, game[5..colonIndex], 10);
             validGames += gameIndex;
         }
     }
     print("Part 1: There are {} valid games\n", .{validGames});
+    print("Part 2: The power sum is {}\n", .{powerSum});
 }
 
 // Useful stdlib functions
